4303e88cba86f692b1a38c54f5b9373f0e2c5504
mstorino <maggiestorino@gmail.com>
2019-12-02 20:45:09 -0600

    first commit

--- src/App.js
+++ src/App.js
@@ -17,7 +17,7 @@ class App extends Component {
             target="_blank"
             rel="noopener noreferrer"
           >
-            Learn React
+            Hi Maggie
           </a>
         </header>
       </div>
